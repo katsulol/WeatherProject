a20657e9360d958517a9994eb957e25095d6eb1a
katsulol <69ferry69thoms69@gmail.com>
2024-05-17 23:12:00 +0530

    Customtkinter 2nd instance

--- Weather.py
+++ Weather.py
@@ -1,15 +1,44 @@
 import requests as req
 from key import key
+import customtkinter as ctk
 
-inp = input("Enter your Place: ")
+class SimpleApp(ctk.CTk):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.title("Weather App")
+        self.geometry("400x400")
 
-data = req.get(f"https://api.openweathermap.org/data/2.5/weather?q={inp}&units=metric&APPID={key}")
+        self.info_label = ctk.CTkLabel(self, text="Write the name of the City.")
+        self.info_label.pack(pady=10)
 
-if data.json()['cod'] == '404':
-    print("No City Found")
-else:
-    weather = data.json()['weather'][0]['main']
-    temp = round(data.json()['main']['temp'])
+        self.user_input = ctk.CTkEntry(self)
+        self.user_input.pack()
 
-    print(f"The weather in {inp} is: {weather}")
-    print(f"The temperature in {inp} is: {temp}ºC")
+
+        self.get_weather_button = ctk.CTkButton(self, text="Get Weather", command=self.get_weather)
+        self.get_weather_button.pack(pady=10)
+
+        self.result_text = ctk.CTkTextbox(self, height=200, width=300)
+        self.result_text.pack(pady=10)
+
+    def get_weather(self):
+        city = self.user_input.get()
+
+        data = req.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&APPID={key}")
+
+        if data.json()['cod'] == '404':
+            self.result_text.delete("1.0", "end")
+            self.result_text.insert("end", "No City Found")
+        else:
+            weather = data.json()['weather'][0]['main']
+            temp = round(data.json()['main']['temp'])
+
+            result = f"The weather in {city} is: {weather}\n"
+            result += f"The temperature in {city} is: {temp}ºC"
+
+            self.result_text.delete("1.0", "end")
+            self.result_text.insert("end", result)
+
+
+app = SimpleApp()
+app.mainloop()
